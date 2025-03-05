c8f293aa0b97a110705e956dfc9c8042f9517827
kaodomi <151603952+kaodomi@users.noreply.github.com>
2025-03-05 16:35:08 +0800

    Add files via upload

---
+++ Lab1/myhello/views.py
@@ -0,0 +1,23 @@
+'''from django.http import HttpResponse
+
+# Create your views here.
+def myIntex(request):
+    my_name = request.POST.get('name', "CGU")
+    return HttpResponse("Hello" + my_name)'''
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class HelloApiView(APIView):
+    def get(self, request):
+        my_name = request.GET.get('name', None)
+        if my_name:
+            retValue = {}
+            retValue['data'] = "Hello" + my_name
+            return Response(retValue, status = status.HTTP_200_OK)
+        else:
+            return Response(
+                {"res": "parameter: name is None"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
