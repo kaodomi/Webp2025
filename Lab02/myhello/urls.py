4d8bb521eb99f936fa527ce77b87c3b5cb2a556a
kaodomi <151603952+kaodomi@users.noreply.github.com>
2025-03-12 15:42:08 +0800

    Add files via upload

---
+++ Lab02/myhello/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #path('', views.HelloApiView.as_view(), name='index'),
+    path('add', views.add_post, name='add_post'),
+    path('list', views.list_post, name='list_post'),
+]
